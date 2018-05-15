2ce004bdf3036c5074eed8ddd00eb62b34888a13
Brandon <bhelms85@gmail.com>
2018-05-14 20:47:00 -0400

    added Breadcrumbs

--- app/routes.py
+++ app/routes.py
@@ -1,38 +1,67 @@
 #!/usr/bin/python3
 
 import os
-from flask import Flask, Blueprint, render_template, redirect, url_for, request, session
+from flask import render_template, session, redirect, url_for, Flask
+from flask_breadcrumbs import Breadcrumbs, register_breadcrumb
 
+"""
+    Building out the application object [app].  Need to rename 'app' to something that makes sense.
+    Can do this later.
+"""
 app = Flask(__name__)
-app.secret_key = os.urandom(24)
+
+""" 
+    Separating out the Login, Logout, and Home pages from the rest of the pages.  These are the only 
+    pages that will not require a session prior to rendering.
+"""
+
 
 @app.route('/')
 @app.route('/home')
 @app.route('/index')
+@register_breadcrumb(app, '.', 'Home')
 def home():
-    print "made it to index"
     return render_template('index.html')
 
+
 @app.route('/login', methods=['GET', 'POST'])
+@register_breadcrumb(app, '.', 'Login')
 def login():
     session['logged_in'] = True
-    session['sidebar_collapse'] = False
     return redirect(url_for('app.home'))
 
+
 @app.route('/logout')
+@register_breadcrumb(app, '.', 'Logout')
 def logout():
     session.clear()
     return redirect(url_for('app.login'))
 
+
+"""
+    Template pages that require a valid session token.  Currently this is not implemented because my last attempt
+    was too clunky.  Currently rethinking how to handle this in a first class.
+"""
+
+
 @app.route('/alerts')
+@register_breadcrumb(app, '.', 'Alerts')
 def alerts():
     return render_template('alerts.html')
 
 
 if __name__ == '__main__':
-    '''
-    This is to enable debug testing of routes directly in Flask.  Not for production run through nginx/uwsgi.
-    '''
-    print "starting up"
+    """
+        This is to enable debug testing of routes directly in Flask.  Not for production run through nginx/uwsgi.
+        All print statements are debug. 
+        
+        Notes:
+        
+        [*] Secret key being set to random will render all sessions invalid upon app restart.  Design decision erroring
+            on security vice availability.
+        [*] Need to think about implementing Blueprint to help manage this project.
+    """
+    print("starting up")
     app.secret_key = os.urandom(24)
-    app.run(host='0.0.0.0',port=80, debug=True)
+    Breadcrumbs(app=app)
+    app.run(host='0.0.0.0', port=80, debug=True)
